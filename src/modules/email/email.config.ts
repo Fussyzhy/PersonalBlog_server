71b5164d14bc530098310fcdbc99f106690bdd6b
张昊阳 <2233832509@qq.com>
2025-03-24 13:54:04 +0800

    邮箱smtp配置修改

--- src/modules/email/email.config.ts
+++ src/modules/email/email.config.ts
@@ -1,8 +1,12 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // 加载.env 文件中的环境变量
+
 export default {
-  host: 'smtp.qq.com',  // 你的 SMTP 服务器地址，如 QQ 邮箱、Gmail 等
-  port: 465,  // SMTP 端口，常见端口：465（SSL）、587（TLS）
+  host: process.env.EMAIL_HOST,  // 你的 SMTP 服务器地址，如 QQ 邮箱、Gmail 等
+  port: process.env.EMAIL_PORT,  // SMTP 端口，常见端口：465（SSL）、587（TLS）
   auth: {
-    user: '2233832509@qq.com',  // 你的邮箱
-    pass: 'udgdxrcjlasjdhia', // 你的 SMTP 授权码（不是邮箱密码）
+    user: process.env.EMAIL_USER,  // 你的邮箱
+    pass: process.env.EMAIL_PASSWORD, // 你的 SMTP 授权码（不是邮箱密码）
   },
 };
